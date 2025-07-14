a450be92b5ddff36e2f121f7ded66552f5f2bd85
immortal202344 <ktimox79@gmail.com>
2025-07-15 02:55:49 +0700

    Update ImmortalUi.lua

--- ImmortalUi.lua
+++ ImmortalUi.lua
@@ -1,4 +1,5 @@
 loadstring(game:HttpGet('https://sirius.menu/sirius'))()
+wait(3)
 local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 local Window = Rayfield:CreateWindow({
    Name = "Rayfield",
